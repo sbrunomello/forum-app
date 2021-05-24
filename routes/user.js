e6a24ba0a1ebd6d3fbef4240be58f31654789abe
sbrunomello <sbrunomello@gmail.com>
2021-05-23 23:43:55 -0300

    node modules

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('user', { title: 'Usuario' });
+router.get('/user', function(req, res, next) {
+  res.render('user.html', { title: 'Usuario' });
 });
 
 module.exports = router;
